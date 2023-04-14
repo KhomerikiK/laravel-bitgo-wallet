44a3676a5c4b72f0713c54d327af8f2aae8d9de6
konstantinekhomeriki <khomeriki.dev@gmail.com>
2023-04-15 00:02:07 +0400

    refactor: config descriptions

--- config/bitgo.php
+++ config/bitgo.php
@@ -1,16 +1,86 @@
 <?php
 
 return [
-    'use_mocks' => env('BITGO_USE_MOCKS', false), //for tests
+    /*
+    |--------------------------------------------------------------------------
+    | Use Mocks
+    |--------------------------------------------------------------------------
+    |
+    | This option determines if the application should use mocks for Bitgo
+    | API calls. This is useful for testing purposes.
+    |
+    */
+    'use_mocks' => env('BITGO_USE_MOCKS', false),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Testnet
+    |--------------------------------------------------------------------------
+    |
+    | This option determines if the application should use the Bitgo testnet
+    | instead of the mainnet. Set this to true for testing and development.
+    |
+    */
     'testnet' => env('BITGO_TESTNET', true),
 
+    /*
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    |
+    | This option sets the API key for the Bitgo API.
+    |
+    */
     'api_key' => env('BITGO_API_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This option sets the API prefix for the Bitgo API.
+    |
+    */
     'v2_api_prefix' => 'api/v2/',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Testnet and Mainnet API URLs
+    |--------------------------------------------------------------------------
+    |
+    | These options set the API URLs for the Bitgo testnet and mainnet.
+    |
+    */
     'testnet_api_url' => 'https://app.bitgo-test.com',
     'mainnet_api_url' => 'https://app.bitgo.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Express API URL
+    |--------------------------------------------------------------------------
+    |
+    | This option sets the Express API URL for the Bitgo API.
+    |
+    */
     'express_api_url' => env('BITGO_EXPRESS_API_URL'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Coin
+    |--------------------------------------------------------------------------
+    |
+    | This option sets the default coin for Bitgo API calls.
+    |
+    */
     'default_coin' => env('BITGO_DEFAULT_COIN', 'tbtc'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Webhook Callback URL
+    |--------------------------------------------------------------------------
+    |
+    | This option sets the webhook callback URL for the Bitgo API.
+    |
+    */
     'webhook_callback_url' => env('BITGO_WEBHOOK_CALLBACK'),
 ];
--- src/BitgoServiceProvider.php
+++ src/BitgoServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Support\ServiceProvider;
 use Khomeriki\BitgoWallet\Adapters\BitgoAdapter;
 use Khomeriki\BitgoWallet\Contracts\BitgoAdapterContract;
 
+/**
+ * BitgoServiceProvider is the service provider for the Bitgo Wallet package.
+ */
 class BitgoServiceProvider extends ServiceProvider
 {
     /**
@@ -18,14 +21,37 @@ class BitgoServiceProvider extends ServiceProvider
             __DIR__.'/../config/bitgo.php',
             'bitgo'
         );
-        $this->app->bind(BitgoAdapterContract::class, BitgoAdapter::class);
 
+        $this->registerBitgoAdapter();
+        $this->registerWallet();
+        $this->registerExchangeRate();
+    }
+
+    /**
+     * Register the BitgoAdapter binding.
+     */
+    protected function registerBitgoAdapter(): void
+    {
+        $this->app->bind(BitgoAdapterContract::class, BitgoAdapter::class);
+    }
+
+    /**
+     * Register the Wallet binding.
+     */
+    protected function registerWallet(): void
+    {
         $this->app->bind('Wallet', function () {
             return new Wallet(
                 app(BitgoAdapterContract::class)
             );
         });
+    }
 
+    /**
+     * Register the ExchangeRate binding.
+     */
+    protected function registerExchangeRate(): void
+    {
         $this->app->bind('ExchangeRate', function () {
             return new ExchangeRate(
                 app(BitgoAdapterContract::class)
@@ -47,6 +73,9 @@ class BitgoServiceProvider extends ServiceProvider
         $this->registerHttpMacros();
     }
 
+    /**
+     * Register Http macros for Bitgo API and Bitgo Express API.
+     */
     public function registerHttpMacros()
     {
         $apiUrl = config('bitgo.testnet') ? config('bitgo.testnet_api_url') : config('bitgo.mainnet_api_url');
